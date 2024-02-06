6cf30a0cf8cf96a60698bb442b37990addf69d02
Tom Morara <morara1920@gmail.com>
2024-02-06 04:52:42 +0300

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that returns the list of
+available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """returns the list of available attributes
+    and methods of an object
+    """
+    return dir(obj)

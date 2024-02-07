e706bf2ca627bde8ad065b29888442c6c61f1b28
Tom Morara <morara1920@gmail.com>
2024-02-07 05:04:50 +0300

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Student that defines a
+student by: (based on 9-student.py)
+"""
+
+
+class Student:
+    "class Student that defines a student"
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        filtered_attrs = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_attrs[attr] = getattr(self, attr)
+        return filtered_attrs

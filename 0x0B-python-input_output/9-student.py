a679f826cf89066dc2fbb9ac2bef873fd500781c
Tom Morara <morara1920@gmail.com>
2024-02-07 05:04:28 +0300

    9. Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"class Student that defines a student"
+
+
+class Student:
+    "class Student that defines a student"
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }

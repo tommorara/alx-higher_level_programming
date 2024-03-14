9401533f5ec275bdde9449b88f05b35082a8ac3f
Tom Morara <morara1920@gmail.com>
2024-03-14 05:32:57 +0300

    0. My privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Script to list all privileges of specific MySQL users on localhost
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

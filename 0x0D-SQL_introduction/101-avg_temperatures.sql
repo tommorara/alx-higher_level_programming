820ea2fa8f8a7400f652f8ea9510be888e6ddd68
Tom Morara <morara1920@gmail.com>
2024-03-15 02:56:02 +0300

    average temperatures

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Calculates the average temperature by city, ordered by descending temperature
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
+

3999bd2fc00321fa4c236de84d92799bf9b6e14a
Tom Morara <morara1920@gmail.com>
2024-03-15 03:18:40 +0300

    no genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,5 @@
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- Script to list all shows without a genre linked
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL

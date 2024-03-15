b71deaaff3952cbf1c055ac8aa23520e48df31c3
Tom Morara <morara1920@gmail.com>
2024-03-15 03:16:44 +0300

    genre id all shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- Script to list all shows and their genre IDs, including NULL for shows without a genre
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 

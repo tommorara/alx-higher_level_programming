117b0450ce79738186ee559e275ca7e49eb4f301
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 <morara1920@gmail.com>
2024-04-11 03:39:48 +0300

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -10,4 +10,4 @@ for (let i = 0; i < values.length; i++) {
   matched = dKeys.filter(key => dict[key] === values[i]);
   matched.forEach(item => result[JSON.stringify(values[i])].push(item));
 }
-console.log(result)
+console.log(result);

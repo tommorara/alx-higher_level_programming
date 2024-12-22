863e2575f20a9b6a636a0a8a478c016ff79102c8
Tom Morara <morara1920@gmail.com>
2024-12-22 16:14:04 +0300

    Done

--- 0x00-python-hello_world/self_taught/1_example.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Ask the user to input there name and assign it to a variable named name
-
-name = input("What is your name? ")
-
-# Print out hello followed with the name they entered
-
-print('Hello ', name)

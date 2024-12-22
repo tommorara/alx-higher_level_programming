567b42b9b33b3949742b95e885c147a267d08b33
Tom Morara <morara1920@gmail.com>
2024-12-22 13:41:57 +0300

    revised

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = "ALX" 
-print(f"Welcome to {str1}")
+str1 = f"ALX {str2}".split()[0]  # Use str2 to construct and then extract "ALX"
+print(f"Welcome to {str1}!")
+
+

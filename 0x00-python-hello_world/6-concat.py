f7cf8b575e5ad956f82cc69cd9a7beaa9f1f8868
Tom Morara <morara1920@gmail.com>
2024-12-22 12:54:35 +0300

    revised

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = "ALX"  # Redefine str1 to contain "ALX"
-print(f"Welcome to {str1}!")
-
+str1 += " " + str2
+print(f"Welcome to {str1}!"

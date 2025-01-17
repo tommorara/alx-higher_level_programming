d0c75bf16b21b852204f001703fc027abc01eb33
Tom Morara <morara1920@gmail.com>
2025-01-18 01:17:12 +0300

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""script that fetches https://intranet.hbtn.io/status"""
 
 import urllib.request
 
 if __name__ == "__main__":
-    # Create a request object for the URL
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
-    # Send the request and store the response in a variable
-    with urllib.request.urlopen(request) as response:
-        # Read the response body
-        body = response.read()
-
-        # Print the response information
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as reply:
+        html = reply.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))

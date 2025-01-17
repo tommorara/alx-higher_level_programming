5ccbafcb9df53f4038e3544360a154707ebec01b
Tom Morara <morara1920@gmail.com>
2025-01-18 01:15:41 +0300

    revised

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status."""
+"""script that Fetches the status of the ALX intranet server
+Link: https://alx-intranet.hbtn.io/status"""
 
 import requests
 
 if __name__ == "__main__":
-    # Send a GET request to the URL and store the response in a variable
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    page = requests.get('https://alx-intranet.hbtn.io/status')
 
-    # Print the response information
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text)))
+    print(f"\t- type: {type(page.text)}")
+    print(f"\t- content: {page.text}")

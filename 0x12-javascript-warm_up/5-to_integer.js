da89f7cfb1de5a11dff6c1128d4a9d5401b4bf86
Tom Morara <morara1920@gmail.com>
2025-06-22 20:17:11 +0300

    reviewed 220625

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-const number = parseInt(arg);
-if (isNaN(number)) {
-  console.log('Not a number');
-} else {
+const fisrtArg = process.argv[2];
+const number = parseInt(fisrtArg);
+if (!isNaN(number)) {
   console.log('My number: ' + number);
+} else {
+  console.log('Not a number');
 }

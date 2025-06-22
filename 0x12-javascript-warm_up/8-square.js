61b8bf722cebd79d64bc3d54083b8260ee60b187
Tom Morara <morara1920@gmail.com>
2025-06-22 23:00:44 +0300

    220625

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing size');
-} else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('X'.repeat(x));
-    i++;
+const firstArg = process.argv[2];
+const x = parseInt(firstArg);
+if (!isNaN(x)) {
+  for (let i = 1; i <= x; i++) {
+    console.log('x'.repeat(x));
   }
+} else {
+  console.log('Missing size');
 }

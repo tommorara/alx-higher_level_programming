61b4e9ab85d31598877242e519488b4927eebcce
Tom Morara <morara1920@gmail.com>
2025-06-22 21:01:22 +0300

    reviewed on 220625

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 while (i < lines.length) {

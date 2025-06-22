6384b5aea7e790d28b1f0b27bd3af38dc8d9fb25
Tom Morara <morara1920@gmail.com>
2025-06-22 22:22:33 +0300

    reviewed on 220625

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
-} else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+const firstArg = process.argv[2];
+const x = parseInt(firstArg);
+if (!isNaN(x)) {
+  for (let i = 1; i <= x; i++) {
     console.log('C is fun');
-    i++;
   }
+} else {
+  console.log('Missing number of occurances');
 }

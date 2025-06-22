f510a8f571a4f478e197e58ae9f0252109d73c0a
Tom Morara <morara1920@gmail.com>
2025-06-22 23:58:18 +0300

    220625

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function add (a, b) {
-  const c = a + b;
-  console.log(c);
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+function add(a,b) {
+  console.log(a+b);
 }
-
-add(Number(process.argv[2]), Number(process.argv[3]));
+add (a,b);

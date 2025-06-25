b21c0a103c2d11d412556387724f8dadae5d6031
Tom Morara <morara1920@gmail.com>
2025-06-25 08:41:08 +0300

    250625

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,6 +2,11 @@
 const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
 function add (a, b) {
-  console.log(a + b);
+  if (!isNaN(a) & !isNaN(b)) {
+    console.log(a + b);
+  } else {
+    console.log(a + b);
+  }
 }
+
 add(a, b);

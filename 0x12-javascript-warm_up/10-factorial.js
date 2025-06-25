cae2eab43c3c4e4b49f09d790d1643978d4df0ef
Tom Morara <morara1920@gmail.com>
2025-06-25 15:34:17 +0300

    250625

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2]);
 function factorial (n) {
-  if (isNaN(n) || n < 0 || n === 0) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  } else if (n === 0) {
     return 1;
   } else {
     return n * factorial(n - 1);

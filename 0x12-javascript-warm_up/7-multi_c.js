7fe3327aa4ca0c363945df7c4f2467b19c80a7b5
Tom Morara <morara1920@gmail.com>
2025-06-25 07:49:10 +0300

    250625

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,10 @@
 const x = Number(process.argv[2]);
 let i = 1;
 if (!isNaN(x)) {
-  while(i <= x) {
-    console.log('C is fun'); 
+  while (i <= x) {
+    console.log('C is fun');
     i++;
   }
 } else {
   console.log('Missing number of occurrences');
 }
-  

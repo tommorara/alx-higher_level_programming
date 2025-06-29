c5061621a1dbe7ffa024544d925b78d3e7ac4dad
Tom Morara <morara1920@gmail.com>
2025-06-29 12:37:08 +0300

    290625

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i =0; i < lines.length; i++) {
-  console.log(lines[i])
+const lang = ['C is fun','Python is cool','JavaScript is amazing'];
+for (let i=0; i< lang.length; i++) {
+  console.log(lang[i]);
 }
-

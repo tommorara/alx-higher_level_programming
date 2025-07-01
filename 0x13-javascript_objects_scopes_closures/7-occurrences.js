09c3ad9152614de948f8d3915b13f872d71ebbcb
Tom Morara <morara1920@gmail.com>
2025-07-01 20:46:19 +0300

    010725

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0);
+  return list.filter(item => item === searchElement).length;
 };

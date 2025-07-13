81b9a50449defd8e40127bbe41cde2cdf1c34a76
Tom Morara <morara1920@gmail.com>
2025-07-13 14:18:39 +0300

    130725

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w,h) {
-    if(w > 0 && h > 0) {
+    if(w>0 && h>0) {
       this.width = w;
       this.height = h;
     }
   }
-  print() {
-    for(let i = 1; i <= this.height; i++) {
-    console.log('x'.repeat(this.width));
+    print() {
+    for(let i=1; i<= this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
     }
   }
-}
 module.exports = Rectangle;
+

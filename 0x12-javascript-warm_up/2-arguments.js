922f57e7bbad4c2c7b73adc179ae55e9a504c3f3
Tom Morara <morara1920@gmail.com>
2025-08-03 17:09:17 +0300

    030825

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const args = process.argv.length;
 if (args === 3) {
   console.log('Argmument found');
 } else if (args === 2) {
-  console.log('No argmuent');
+  console.log('No argument');
 } else {
   console.log('Arguments found');
 }
